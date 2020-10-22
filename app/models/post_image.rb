e2948c5ea53a8cde2366dde0aff221ec54c635c0
vagrant <vagrant@localhost.localdomain>
2020-10-22 16:40:06 +0000

    User show・editページ作成

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,10 @@ class PostImage < ApplicationRecord
 	default_scope -> { order(created_at: :desc) }
 	has_many :comments, dependent: :destroy
 	has_many :favorites, dependent: :destroy
+
+	validates :shop_name, presence: true
+  	validates :image, presence: true
+
 	def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   	end
